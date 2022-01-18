4d8bcc461f7ac0068bf73788fffde540010b45c0
scott <scottsuk0306@kaist.ac.kr>
2022-01-19 02:40:52 +0900

    Get input from user

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("hi");
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input")
+const longinButton = loginForm.querySelector("button")
+
+function onLoginBtnClick() {
+    console.dir(loginInput)
+}
+
+longinButton.addEventListener("click", onLoginBtnClick)
